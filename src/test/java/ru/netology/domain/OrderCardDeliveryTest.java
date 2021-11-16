afff71b07c4939f3e0af6b07ecd4d790e1f71eec
irikras <irikras@mail.ru>
2021-11-16 19:36:17 +0300

    Allure add

--- src/test/java/ru/netology/domain/OrderCardDeliveryTest.java
+++ src/test/java/ru/netology/domain/OrderCardDeliveryTest.java
@@ -1,5 +1,9 @@
 package ru.netology.domain;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -14,6 +18,16 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class OrderCardDeliveryTest {
     private RegistrationByCardInfo registrationByCardInfo;
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setUp() {
